3827d2533bfe406b52632351b081e150d549a9de
Shubhang Srikoti <70000106+shubhangsrikoti@users.noreply.github.com>
2020-09-10 19:48:28 +0000

    prinr line

--- main.py
+++ main.py
@@ -5,43 +5,33 @@
 # Section: 4
 # Breakout: 12
 
-def run(grade):
-  getLetterGrade();
-  G = float(input("Enter your CMPSC 131 grade: "));
+
   
 def getLetterGrade(grade):
   if grade >= 93.0:
     G = "A"
-    print("Your letter grade for CMPSC 131 is A.")
   elif grade >= 90.0:
     G = "A-"
-    print("Your letter grade for CMPSC 131 is A-.")
   elif grade >= 87.0:
     G = "B+"
-    print("Your letter grade for CMPSC 131 is B+.")
   elif grade >= 83.0:
     G = "B"
-    print("Your letter grade for CMPSC 131 is B.")
   elif grade >= 80.0:
     G = "B-"
-    print("Your letter grade for CMPSC 131 is B-.")
   elif grade >= 77.0:
     G = "C+"
-    print("Your letter grade for CMPSC 131 is C+.")
   elif grade >= 70.0:
     G = "C"
-    print("Your letter grade for CMPSC 131 is C.")
   elif grade >= 60.0:
     G = "D"
-    print("Your letter grade for CMPSC 131 is D.")
   else :
     G = "F"
-    print("Your letter grade for CMPSC 131 is F.")
   return G;
 
-
-
-  
+def run(grade):
+  getLetterGrade();
+  G = float(input("Enter your CMPSC 131 grade: "));
 
 if __name__ == "__main__":
-  run(getLetterGrade(grade));
+  run(getLetterGrade(grade))
+  print("Your letter grade for CMPSC 131 is: ", G)
