33e35bf64e8e55ade4475f21ed42ed97e16b3eac
Shubhang Srikoti <70000106+shubhangsrikoti@users.noreply.github.com>
2020-09-10 18:32:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Author: Shubhang Srikoti svs6959@psu.psu.edu
+# Collaborator: Aditya Majumdar apm6483@psu.psu.edu
+# Collaborator: Manav Shah mvs6909@psu.edu
+# Collaborator: Weijun Fan wmf5111@psu.edu
+# Section: 4
+# Breakout: 12
+
+def getLetterGrade(grade):
+  if grade >= 93.00:
+    G = "A"
+  elif grade >= 90.00:
+    G = "A-"
+  elif grade >= 87.00:
+    G = "B+"
+  elif grade >= 83.00:
+    G = "B"
+  elif grade >= 80.00:
+    G = "B-"
+  elif grade >= 77.00:
+    G = "C+"
+  elif grade >= 70.00:
+    G = "C"
+  elif grade >= 60.00:
+    G = "D"
+  else :
+    G = "F"
+  return G;
+
+
+def run():
+  getLetterGrade(grade);
+
+if __name__ == "__main__":
+  run():
